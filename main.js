df765153e93558054a9ddc8036b2f2a16e3e2c53
naman kalkhuria <naman@launchyard.com>
2015-07-05 03:26:43 +0530

    some changes

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 function func($item) {
-	alert($item.text());
+	alert($item.text() +' id:'+ $item.data('id'));
 }
 
-var dropdown = new $('.mydropdown').dropdown({defaultText: 'defaultText'});
-dropdown.addItems({selected: {'name': 'as'}, list:[{'name': 'pq'}], trigger: func});
+function func1($item) {
+	alert($item.text() +' id:'+ $item.data('id'));
+}
 
-var dropdown1 = new $('.mydropdown1').dropdown({defaultText: 'defaultText'});
-dropdown1.addItems({selected: {'name': 'as'}, list:[{'name': 'pq'}], trigger: func});
+var dropdown = new $('.mydropdown').dropdown({defaultText: 'Select Item'});
+
+// emulating for response coming from server
+setTimeout(function() {
+	dropdown.addItems({
+		selected: {'name': 'Item1', 'id': '1'}, 
+		list:[
+			{'name': 'Item1', 'id': '1'},
+			{'name': 'Item2', 'id': '2'},
+			{'name': 'Item3', 'id': '3'},
+		],
+		trigger: func
+	});	
+}, 1000);
+
+var dropdown1 = new $('.mydropdown1').dropdown({defaultText: 'Select Item'});
+dropdown1.addItems({
+	selected: {'display_name': 'Item1', '_id': '1'}, 
+	list:[
+		{'display_name': 'Item1', '_id': '1'},
+		{'display_name': 'Item2', '_id': '2'},
+		{'display_name': 'Item3', '_id': '3'},
+	],
+	idAttr: '_id', // define if using other than id
+	nameAttr: 'display_name', // define if using other than name
+	trigger: func1
+});	
