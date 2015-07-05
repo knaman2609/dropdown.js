648b02b34fdc0afdc52add6cabc28855c9bc6fc6
naman kalkhuria <naman@launchyard.com>
2015-07-05 19:35:01 +0530

    code refactor

--- jquery.dropdown.js
+++ jquery.dropdown.js
@@ -1,88 +1,126 @@
 "use strict";
 
 (function($) {
-$.fn.dropdown = function(options) {
-  var root = this,
-    dropdownSel = '<div class="dropdown_sel">'+ options.defaultText+'</div>',
-    dropdownArrow = '<div class="dropdown_arrow"></div>',
-    obj = {};
 
-  function eachItem(selected, item, id, name) {
-    var temp = '';
+function eachItem(selected, item, id, name) {
+  var temp = '';
 
-    if (typeof item[id] == 'undefined') {
-     return '<li class="dropdown_item">'+ item[name] +'</li>';  
-    }
-
-    if (selected[id] == item[id])
-    temp = '<li class="dropdown_item dropdown_item-selected" data-id='+  item[id] +'>'+ item[name] +'</li>';
-    else
-    temp = '<li class="dropdown_item" data-id='+ item[id] +'>'+ item[name] +'</li>';  
-
-    return temp;
-  } 
-
-  var addItems = function(options) {
-    var content = '', 
-      id = options.idAttr || 'id',
-      name = options.nameAttr || 'name',
-      items = '',
-      dropdownSel = '<div class="dropdown_sel">'+ options.selected[name]+'</div>';
-      
-    content += dropdownSel;
-    for (var i=0 ;i< options.list.length; i++) {
-      items += eachItem(options.selected, options.list[i], id, name);
-    }
-    
-    content += '<ul class="dropdown_list is-hidden">'+ items +'</ul>';
-    
-    this.root.find('.dropdown_sel').remove();
-    this.root.append(content);    
-
-    if (!this.init) {
-
-      this.root.click(function(e) {
-        var $target = $(e.target),
-          close,
-          parent;
-
-        if ($target.hasClass('dropdown_sel')) {
-          close = $target.next().hasClass('is-hidden');
-          $('.dropdown_list').addClass('is-hidden');
-          if (close) {
-            $target.next().removeClass('is-hidden');
-          }
-          else {
-            $target.next().addClass('is-hidden');
-          }
-        }
-      
-        if ($target.hasClass('dropdown_item')) {
-          parent = $target.closest('.dropdown');
-        
-          parent.find('.dropdown_sel').text($target.text());
-          parent.find('.dropdown_list').addClass('is-hidden');
-          parent.find('.dropdown_item').removeClass('dropdown_item-selected');
-          $target.addClass('dropdown_item-selected');
-          options.trigger($target, parent);
-        }
-        e.stopPropagation();
-      });  
-
-      $(document).click(function() {
-        $('.dropdown_list').addClass('is-hidden');
-      });
-      this.init = true;
-    }
+  if (typeof item[id] == 'undefined') {
+   return '<li class="dropdown_item">'+ item[name] +'</li>';  
   }
-    
-  root.addClass('dropdown');
-  root.append(dropdownSel + dropdownArrow);
-  obj.root = root;
-  obj.addItems = addItems;
 
-  return obj;
+  if (selected[id] == item[id])
+  temp = '<li class="dropdown_item dropdown_item-selected" data-id='+  item[id] +'>'+ item[name] +'</li>';
+  else
+  temp = '<li class="dropdown_item" data-id='+ item[id] +'>'+ item[name] +'</li>';  
+
+  return temp;
+} 
+
+var Dropdown = function(element, options) {
+  this.$element = element;
+  this.options = options;
+  this.create();
+  this.addListeners();
 }
+
+Dropdown.prototype.create = function() {
+  var dropdownSel = '<div class="dropdown_sel">'+ this.options.defaultText+'</div>',
+    dropdownArrow = '<div class="dropdown_arrow"></div>';
+
+  this.$element.addClass('dropdown');  
+  this.$element.html(dropdownSel + dropdownArrow);
+}
+
+Dropdown.prototype.open = function() {
+  this.$element.find('.dropdown_list').removeClass('is-hidden');
+}
+
+Dropdown.prototype.close = function() {
+  this.$element.find('.dropdown_list').addClass('is-hidden');
+}
+
+Dropdown.prototype.selectedItem = function($item) {
+  this.$element.find('.dropdown_sel').text($item.text());
+  this.$element.find('.dropdown_item').removeClass('dropdown_item-selected');
+  $item.addClass('dropdown_item-selected');
+  this.$element.find('.dropdown_list').addClass('is-hidden');
+
+  if (this.options.trigger)
+  this.options.trigger($item, this.$element);
+}
+
+Dropdown.prototype.addListeners = function() {
+  var self = this;
+
+  this.$element.on('click', function(e) {
+    e.stopPropagation();
+
+    var $target = $(e.target),
+      close;
+
+    if ($target.hasClass('dropdown_sel')) {
+      close = self.$element.find('.dropdown_list').hasClass('is-hidden');
+
+       // hide all dropdowns
+      $('.dropdown_list').addClass('is-hidden');
+
+      if (close) self.open();
+      else self.close();
+    }
+
+    if ($target.hasClass('dropdown_item')) 
+    self.selectedItem($target);
+
+    e.stopPropagation();
+  });  
+
+  $(document).click(function() {
+    $('.dropdown_list').addClass('is-hidden');
+  });
+}
+
+
+
+Dropdown.prototype.addItems = function(options) {
+  var content = '', 
+    id = options.idAttr || 'id',
+    name = options.nameAttr || 'name',
+    items = '',
+    dropdownSel = '<div class="dropdown_sel">'+ options.selected[name]+'</div>';
+    
+  content += dropdownSel;
+  for (var i=0 ;i< options.list.length; i++) {
+    items += eachItem(options.selected, options.list[i], id, name);
+  }
+  
+  content += '<ul class="dropdown_list is-hidden">'+ items +'</ul>';
+  
+  this.$element.find('.dropdown_sel').remove();
+  this.$element.append(content);    
+}
+
+// plugin defaults
+Dropdown.DEFAULTS = {
+  defaultText: 'Test',
+  trigger: null
+}
+
+// creates the Plugin
+function Plugin(option) {
+    var data  = this.data('plugin.dropdown'),
+    options = $.extend({}, Dropdown.DEFAULTS, this.data(), option),
+    dropdown;
+
+    if (!data) {
+      dropdown =  new Dropdown(this, options);
+      this.data('plugin.dropdown', (data = dropdown));
+    }
+
+    return dropdown;
+}
+
+$.fn.dropdown = Plugin;
 })(jQuery) 
 
 
--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-function func($item) {
-	alert($item.text() +' id:'+ $item.data('id'));
-}
+var a = $('.mydropdown').dropdown({
+			defaultText: 'Select Item', 
+			trigger: function($Item) {
+				alert($Item.text() + ' id:'+ $Item.data('id'));
+			}
+		});
 
-function func1($item) {
-	alert($item.text() +' id:'+ $item.data('id'));
-}
+var b = $('.mydropdown1').dropdown({
+			defaultText: 'Select Item', 
+			trigger: function($Item) {
+				alert($Item.text() + ' id:'+ $Item.data('id'));
+			}
+		});
 
-var dropdown = new $('.mydropdown').dropdown({defaultText: 'Select Item'});
-
-// emulating for response coming from server
-setTimeout(function() {
-	dropdown.addItems({
-		selected: {'name': 'Item1', 'id': '1'}, 
-		list:[
-			{'name': 'Item1', 'id': '1'},
-			{'name': 'Item2', 'id': '2'},
-			{'name': 'Item3', 'id': '3'},
-		],
-		trigger: func
-	});	
-}, 1000);
-
-var dropdown1 = new $('.mydropdown1').dropdown({defaultText: 'Select Item'});
-dropdown1.addItems({
-	selected: {'display_name': 'Item1', '_id': '1'}, 
+a.addItems({
+	selected: {'name': 'Item3', 'id': '3'}, 
 	list:[
-		{'display_name': 'Item1', '_id': '1'},
-		{'display_name': 'Item2', '_id': '2'},
-		{'display_name': 'Item3', '_id': '3'},
-	],
-	idAttr: '_id', // define if using other than id
-	nameAttr: 'display_name', // define if using other than name
-	trigger: func1
-});	
+	    {'name': 'Item1', 'id': '1'},
+	    {'name': 'Item2', 'id': '2'},
+	    {'name': 'Item3', 'id': '3'},
+	]
+}); 
+
+b.addItems({
+	selected: {'name': 'Item1', 'id': '1'}, 
+	list:[
+	    {'name': 'Item1', 'id': '1'},
+	    {'name': 'Item2', 'id': '2'},
+	    {'name': 'Item3', 'id': '3'},
+	]
+}); 
